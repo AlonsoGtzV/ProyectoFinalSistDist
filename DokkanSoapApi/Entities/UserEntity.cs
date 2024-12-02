8dd37044eb41eccf147a424ed25b829d49adaeb6
Alonso Gutiérrez <114546823+AlonsoGtzV@users.noreply.github.com>
2024-12-01 23:57:29 -0600

    Delete DokkanSoapApi directory

--- DokkanSoapApi/Entities/UserEntity.cs
+++
@@ -1,9 +0,0 @@
-namespace DokkanSoapApi.Infrastructure.Entities;
-
-public class UserEntity{
-    public Guid Id {get; set;}
-    public string username {get; set;} = null!;
-    public string level {get; set;} =null!;
-    public string powerLevel {get; set;} =null!;
-
-}
--- DokkanSoapApi/Models/UserModel.cs
+++
@@ -1,8 +0,0 @@
-namespace DokkanSoapApi.Models;
-
-public class UserModel{
-    public Guid Id {get; set;}
-    public string username {get; set;} = null!;
-    public string level {get; set;}=null!;
-    public string powerLevel {get; set;} =null!;
-}
